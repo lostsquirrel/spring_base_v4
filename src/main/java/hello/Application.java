67f1f7c63c4f0fb76bad47f7905be4dce57643f6
lisong <songli1988@gmail.com>
2016-03-10 22:02:25 +0800

    create spring demo with java configuration

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -20,6 +20,7 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 		MessagePrinter printer = context.getBean(MessagePrinter.class);
 		printer.printMessage();
