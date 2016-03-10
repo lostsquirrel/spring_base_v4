8e17d1104a784a15808316a69b976d538315ff76
lisong <songli1988@gmail.com>
2016-03-10 22:34:13 +0800

    add spring base demo configuration with xml

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,26 +1,14 @@
 package hello;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class Application {
 	
-	@Bean
-	MessageService mockMessageService() {
-		return new MessageService() {
-			public String getMessage() {
-				return "Hello World!";
-			}
-		};
-	}
-
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+		@SuppressWarnings("resource")
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext(new String[] {"application-context.xml"});
 		MessagePrinter printer = context.getBean(MessagePrinter.class);
 		printer.printMessage();
 	}
